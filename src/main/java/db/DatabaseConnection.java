4abfe0775127a9305cd54f0f1116dc3351edbfe0
Manishadh10 <152870634+Manishadh10@users.noreply.github.com>
2025-06-25 21:25:20 +0545

    DatabaseConnection.java

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/quiz_game";
     private static final String USER = "root"; // Replace with your DB username
-    private static final String PASSWORD = "m@nish@dh@631230"; // Replace with your DB password
+    private static final String PASSWORD = ""; // Replace with your DB password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
